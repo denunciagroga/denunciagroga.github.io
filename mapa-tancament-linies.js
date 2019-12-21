866c0ea18e903a3fa30fceed515bb03bba376e83
mfidemraizer <fidemraizer@hotmail.com>
2019-12-21 17:19:54 +0100

    xx

---
+++ mapa-tancament-linies.js
@@ -0,0 +1,38 @@
+mapaTancamentsMarkers = [
+    {
+     name: "CEIP Balandrau",
+     courses: [
+         {
+             name: "2020-2021",
+             text: 'Tancament de P3',
+             url: 'https://twitter.com/totssombalandr1/status/1207949323985666048'
+         }
+     ],
+     lat: 2.809418,
+     long: 41.991528
+    },
+    {
+        name: "CEIP Auró / President Salvans",
+        courses: [
+            {
+                name: "2020-2021",
+                text: "Tancament d'una línia de primària",
+                url: 'https://elmon.cat/monterrassa/general/educacio-vol-tancar-linia-primaria-lescola-auro-terrassa'
+            }
+        ],
+        lat: 2.021484,
+        long: 41.553811
+    },
+    {
+        name: "CEIP Torrent d'en Melis",
+        courses: [
+            {
+                name: "2019-2020",
+                text: "Tancament de P3",
+                url: 'https://twitter.com/P3Torrent/status/1141688608493096961'
+            }
+        ],
+        lat: 2.169969,
+        long: 41.417303
+    }
+]
