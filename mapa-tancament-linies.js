de8f7cf5ed36815d95b48976b6722dc3432b52db
mfidemraizer <fidemraizer@hotmail.com>
2019-12-29 20:43:12 +0100

    xx

--- mapa-tancament-linies.js
+++ mapa-tancament-linies.js
@@ -1,63 +1,21 @@
+---
+---
+
 mapaTancamentsMarkers = [
-    // GIRONA
+{% for centre in site.data.tancamentLinies %}
     {
-     name: "CEIP Balandrau",
-     courses: [
-         {
-             name: "2020-2021",
-             text: 'Tancament de P3',
-             url: 'https://twitter.com/totssombalandr1/status/1207949323985666048'
-         }
-     ],
-     lat: 2.809418,
-     long: 41.991528
-    },
-    {
-        name: "CEIP Montfalgars",
-        lat: 2.800072,
-        long: 41.9731408,
+        name: "{{ centre.name }}",
+        lat: {{ centre.lat }},
+        long: {{ centre.long }},
         courses: [
-            {
-                name: '2020-2021',
-                text: 'Tancament de P3',
-                url: 'https://www.diaridegirona.cat/girona/2019/12/24/ccoo-soposa-al-tancament-linies/1020531.html'
-            }
+            {% for course in centre.courses %}
+                {
+                    name: "{{ course.name }}",
+                    text: "{{ course.text }}",
+                    url: "{{ course.url }}"
+                }
+            {% endfor %}
         ]
     },
-    {
-        name: "CEIP Marta Mata",
-        lat: 2.8121138,
-        long: 41.9643905,
-        courses: [
-            {
-                name: '2020-2021',
-                text: 'Tancament de P3',
-                url: 'https://www.diaridegirona.cat/girona/2019/12/24/ccoo-soposa-al-tancament-linies/1020531.html'
-            }
-        ]
-    },
-    {
-        name: "CEIP Auró / President Salvans",
-        courses: [
-            {
-                name: "2020-2021",
-                text: "Tancament d'una línia de primària",
-                url: 'https://elmon.cat/monterrassa/general/educacio-vol-tancar-linia-primaria-lescola-auro-terrassa'
-            }
-        ],
-        lat: 2.021484,
-        long: 41.553811
-    },
-    {
-        name: "CEIP Torrent d'en Melis",
-        courses: [
-            {
-                name: "2019-2020",
-                text: "Tancament de P3",
-                url: 'https://twitter.com/P3Torrent/status/1141688608493096961'
-            }
-        ],
-        lat: 2.169969,
-        long: 41.417303
-    }
+{% endfor %}
 ]
